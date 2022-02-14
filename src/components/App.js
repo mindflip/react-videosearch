d58a541ad4c2415134e8113cb9c9339a5074f855
Eomy <hmu1212@hotmail.com>
2022-02-14 18:06:29 +0900

    [feat] video detail

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import useVideos from "../hooks/useVideos";
 import SearchBar from "./SearchBar";
+import VideoDetail from "./VideoDetail";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
@@ -14,6 +15,14 @@ const App = () => {
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">list</div>
+        </div>
+      </div>
     </div>
   );
 };
